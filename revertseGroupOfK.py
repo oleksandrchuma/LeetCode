b167d7b81765eee1ca33ac5be326b9ad89e85e5f
Sasha Chuma <sanya.chuma@gmail.com>
2024-01-05 10:15:53 +0200

    reverse linked list

---
+++ revertseGroupOfK.py
@@ -0,0 +1,41 @@
+from typing import List
+from typing import Optional
+import time
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        curr = head
+        fakehead = ListNode()
+        prev = fakehead
+        while (curr):
+            store = []
+            while (curr and len(store) < k):
+                store.append(curr)
+                curr = curr.next
+            if (len(store) == k):
+                while(len(store) > 0):
+                    prev.next = store.pop()
+                    prev = prev.next
+                prev.next = curr    
+        return fakehead.next if fakehead.next else head
+    
+    def print_linked_list(self, head):
+        current = head
+        while current is not None:
+            print(current.val, end=" -> ")
+            current = current.next
+        print("None")
+        
+list1 = ListNode(4, ListNode(0, ListNode(2, ListNode(5))))
+start_time = time.time()
+#res = Solution().mergeKLists([list1])
+res=Solution().reverseKGroup(list1, 4)
+Solution().print_linked_list(res)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time} seconds")
