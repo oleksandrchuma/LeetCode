1f27fdd6519eb7ebdf021627c62202ef7c869ad1
Sasha Chuma <sanya.chuma@gmail.com>
2024-02-08 13:34:08 +0200

    279. added nonrecursive solution (dp)

--- 279. Perfect squares.py
+++ 279. Perfect squares.py
@@ -6,7 +6,7 @@ from collections import Counter
 from functools import lru_cache
 
 class Solution:
-    def numSquares(self, n: int) -> int:
+    def numSquares2(self, n: int) -> int:
         @lru_cache(None)
         def numSquaresRec(num):
             res = num
@@ -23,8 +23,24 @@ class Solution:
             x += 1
         
         return numSquaresRec(n)
+
+    def numSquares(self, n: int) -> int:
+        squares = []
+        x = 2 
+        while (x*x <= n):
+            squares.append(x*x)
+            x += 1
+        res = [i for i in range(0, n+1)]
+        for i in range(2, n+1):
+            for k in squares:
+                if k > i:
+                    break
+                if res[i-k] + 1 < res[i]:
+                    res[i] = res[i-k] + 1
+        return res[-1]
 start_time = time.time()
 app = Solution()
+print(app.numSquares(13))
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time} seconds")
